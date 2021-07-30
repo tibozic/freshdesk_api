b240bc9db6cb2f49a0b82861afa7f0dd98ac681e
Timotej Božič <timotejbozic.tb@gmail.com>
2021-07-30 19:28:52 +0200

    Casovni interval

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call("App\Http\Controllers\TicketsController@database_update")->everyMintue();
     }
 
     /**
